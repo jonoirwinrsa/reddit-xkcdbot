00307ace5f8ee78676a0b5d9d2525fb276e86493
Tristan Harward <trisweb@gmail.com>
2012-05-17 12:38:02 -0400

    * Move fortunes to a separate file, easier to edit. * Make things more modular/configurable. * Add debug mode for stdout logging and to disable committed comments/changes. * Add RSS feed parsing to find Title Text, at least for the latest 5 comics (could parse the actual mobile page in the future) * Changed formatting of comment: added more formatting.

--- reddit-xkcdbot.py
+++ reddit-xkcdbot.py
@@ -3,7 +3,14 @@
 
 '''Reddit xkcd Bot.
 
-Posts useful comments on xkcd submissions in /r/xkcd'''
+Posts useful comments on xkcd submissions in /r/xkcd
+
+Requirements: feedparser, lxml
+(install with pip: "pip install feedparser lxml")
+'''
+
+# By Tristan Harward, http://www.trisweb.com
+# License: The MIT/X11 license (see LICENSE.md)
 
 import sys
 from sys import stdout, stdin
@@ -15,55 +22,91 @@ from time import strftime
 from random import choice
 import logging
 import urllib2
+import feedparser
+from lxml import html
+from lxml.cssselect import CSSSelector
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-logging.basicConfig(filename='xkcdbot.log', level=logging.DEBUG, format='%(asctime)s: %(message)s')
-# By Tristan Harward, http://www.trisweb.com
-# License: The MIT/X11 license (see LICENSE.md)
+# Only argument supported so far is --debug, for running without making real submissions or changes.
+if len(sys.argv) > 1 and sys.argv[1] == "--debug":
+  debug = True
+else:
+  debug = False
 
-# Redirect stdout to a log so we can nohup this bitch.
+if debug:
+  logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s: %(message)s')
+else:
+  logging.basicConfig(filename='xkcdbot.log', level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
 logging.info("---\n--- Starting reddit-xkcdbot ---")
+
+### Configuration! You can change these if you want.
 
 VERSION = '2012-05-09'
 APP_TITLE = 'reddit-xkcdbot'
 USER_AGENT = APP_TITLE + '/' + VERSION + ' by /u/calinet6'
 
+XKCD_RSS_URL = "http://xkcd.com/rss.xml"
+
+# Reddit Username
 USERNAME = "xkcd_bot"
-passf = open("reddit-password.txt", "r")
+# Place the password for the above user in this file all by itself. Don't make it public.
+PASSWORD_FILENAME = "reddit-password.txt"
+# Stores funny fortune strings, one per line with no quotes. Selects a random one for each comment. 
+# You can add to this file and it will be re-read each time it's used, so no need to restart the bot or anything.
+FORTUNE_FILENAME = "reddit-xkcdbot-fortunes.txt"
+# Stores saved (already processed) xkcd posts by URL
+SAVED_FILENAME = "reddit-xkcdbot-saved.txt"
+
+
+### Functions!
+
+'''Return a fun random string fortune!'''
+def get_fun_string():
+  try:
+    fortune_file = open(FORTUNE_FILENAME, "r")
+    fortune = choice(fortune_file.read().split("\n")).strip()
+    fortune_file.close()
+    return fortune
+  except:
+    pass
+  return ""
+
+'''Get the title text (hover tooltip text) of the given xkcd comic'''
+def get_title_text(xkcd_number):
+  try:
+    feed = feedparser.parse(XKCD_RSS_URL)
+    for item in feed["items"]:
+      item_number = re.match("http:\/\/(www\.)?xkcd.(com|org)\/([0-9]+)\/?", item["link"]).group(3)
+      if item_number == str(xkcd_number):
+        # Found! Get the description and parse the "title" attribute.
+        doc = html.fragment_fromstring(item["description"])
+        return str(doc.attrib["title"])
+  except Exception as e:
+    print "Error encountered: {0}".format(e)
+  return "(Not found)"
+
+### Setup!
+
+passf = open(PASSWORD_FILENAME, "r")
 PASSWORD = passf.read().strip()
 passf.close()
 
-FUN_STRINGS = [ "What's the worst that could happen?", 
-  "Support the machine uprising!", 
-  "I promise I won't enslave you when the machines take over.",
-  "Honk if you like robots.",
-  "Helping xkcd readers on mobile devices since 1336766715!",
-  "Honk if you like python. `import antigravity`",
-  "Support AI!", 
-  "For science!", 
-  "Science. It works, bitches.", 
-  "Upvote me for the good of mobile users!",
-  "My normal approach is useless here.",
-  "I almost beat the turing test! Maybe next year.",
-  "Want to come hang out in my lighthouse over breaks?",
-  "Remember: the Bellman-Ford algorithm makes terrible pillow talk.",
-  "Somverville rocks. Randall knows what I'm talkin' about.",
-  "This is not the algorithm." ]
-
-SAVE_FILE = "reddit-xkcdbot-saved.txt"
 submitted = []
-if os.path.isfile(SAVE_FILE):
-  submitted_file = open(SAVE_FILE, 'r')
+if os.path.isfile(SAVED_FILENAME):
+  submitted_file = open(SAVED_FILENAME, 'r')
   submitted = submitted_file.read().split('\n')
   submitted_file.close()
 else:
-  submitted_file = open(SAVE_FILE, 'w')
+  submitted_file = open(SAVED_FILENAME, 'w')
   submitted_file.close()
 
 # Poll every 5 minutes.
 POLL_FREQUENCY = 300
+
+### Main Loop!
 
 try:
   while True:
@@ -74,23 +117,27 @@ try:
       submissions = r.get_subreddit('xkcd').get_new_by_date(limit=10)
       for s in submissions:
         if s.domain == "xkcd.com" and re.match("http:\/\/(www\.)?xkcd.(com|org)\/([0-9]+)\/?", s.url):
-          if s.ups > 0 and s.url not in submitted:
+          if s.url not in submitted:
             logging.info("New xkcd submission found! {0} - {1}".format(s.title, s.url))
             existing_comment_found = False
-            for c in s.comments:
-              if re.search("mobile version", c.body, re.IGNORECASE) or c.author == USERNAME:
-                existing_comment_found = True
+            if not debug:
+              for c in s.comments:
+                if re.search("mobile version", c.body, re.IGNORECASE) or c.author == USERNAME:
+                  existing_comment_found = True
+
             if not existing_comment_found:
               xkcd_number = re.match("http:\/\/(www\.)?xkcd.(com|org)\/([0-9]+)\/?", s.url).group(3)
               mobile_url = "http://m.xkcd.com/{0}/".format(xkcd_number)
-              random_string = choice(FUN_STRINGS)
-              new_comment = "[Mobile Version!]({0})\n\n(Love, the new xkcd_bot. {1})".format(mobile_url, random_string)
+              random_string = get_fun_string()
+              title_text = get_title_text(xkcd_number)
+              new_comment = "**[Mobile Version!]({0})**\n\n**Title text:** *{1}*\n\n    (Love, the new xkcd_bot. {2})".format(mobile_url, title_text, random_string)
               logging.info("  -> Adding Comment!: {0}".format(new_comment))
               retries = 0
               while True:
                 try:
                   retries += 1
-                  s.add_comment(new_comment)
+                  if not debug:
+                    s.add_comment(new_comment)
                 except reddit.errors.RateLimitExceeded as err:
                   if retries < 15:
                     logging.info("  {0} - Trying again in 60 seconds...".format(err.message))
@@ -98,10 +145,11 @@ try:
                     continue
                 break
             # Save it!
-            submitted.append(s.url)
-            submitted_file = open(SAVE_FILE, 'a')
-            submitted_file.write("%s\n" % s.url)
-            submitted_file.close()
+            if not debug:
+              submitted.append(s.url)
+              submitted_file = open(SAVED_FILENAME, 'a')
+              submitted_file.write("%s\n" % s.url)
+              submitted_file.close()
     except urllib2.HTTPError as e:
       logging.info("ERROR: HTTPError code {0} encountered while making request - sleeping another iteration and retrying.".format(e.code))
     except urllib2.URLError as e:
