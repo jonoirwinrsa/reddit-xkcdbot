828177c08c61ba3678b4cf5492aff41c9c22baac
Tristan Harward <trisweb@gmail.com>
2012-08-31 07:58:49 -0400

    Converted spaces to 4-wide; catch-all for exceptions
    Helps when Reddit goes down and returns weird stuff.

--- reddit-xkcdbot.py
+++ reddit-xkcdbot.py
@@ -31,14 +31,14 @@ sys.setdefaultencoding("utf-8")
 
 # Only argument supported so far is --debug, for running without making real submissions or changes.
 if len(sys.argv) > 1 and sys.argv[1] == "--debug":
-  debug = True
+    debug = True
 else:
-  debug = False
+    debug = False
 
 if debug:
-  logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s: %(message)s')
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s: %(message)s')
 else:
-  logging.basicConfig(filename='xkcdbot.log', level=logging.DEBUG, format='%(asctime)s: %(message)s')
+    logging.basicConfig(filename='xkcdbot.log', level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 logging.info("---\n--- Starting reddit-xkcdbot ---")
 
@@ -56,7 +56,7 @@ ENABLE_TITLE_TEXT = True
 USERNAME = "xkcd_bot"
 # Place the password for the above user in this file all by itself. Don't make it public.
 PASSWORD_FILENAME = "reddit-password.txt"
-# Stores funny fortune strings, one per line with no quotes. Selects a random one for each comment. 
+# Stores funny fortune strings, one per line with no quotes. Selects a random one for each comment.
 # You can add to this file and it will be re-read each time it's used, so no need to restart the bot or anything.
 FORTUNE_FILENAME = "reddit-xkcdbot-fortunes.txt"
 # Stores saved (already processed) xkcd posts by URL
@@ -65,32 +65,34 @@ SAVED_FILENAME = "reddit-xkcdbot-saved.txt"
 
 ### Functions!
 
-'''Return a fun random string fortune!'''
-def get_fun_string():
-  try:
-    fortune_file = open(FORTUNE_FILENAME, "r")
-    fortunes = fortune_file.read().strip().split("\n")
-    fortune = choice(fortunes).strip()
-    fortune_file.close()
-    return fortune
-  except:
-    pass
-  return ""
 
-'''Get the title text (hover tooltip text) of the given xkcd comic'''
+def get_fun_string():
+    '''Return a fun random string fortune!'''
+    try:
+        fortune_file = open(FORTUNE_FILENAME, "r")
+        fortunes = fortune_file.read().strip().split("\n")
+        fortune = choice(fortunes).strip()
+        fortune_file.close()
+        return fortune
+    except:
+        pass
+    return ""
+
+
 def get_title_text(xkcd_number):
-  try:
-    feed = feedparser.parse(XKCD_RSS_URL)
-    for item in feed["items"]:
-      item_number = re.match(URL_REGEX, item["link"], re.IGNORECASE).group(4)
-      if item_number == str(xkcd_number):
-        # Found! Get the description and parse the "title" attribute.
-        doc = html.fragment_fromstring(item["description"])
-        title_text = str(doc.attrib["title"])
-        return title_text
-  except Exception as e:
-    print "Error encountered: {0}".format(e)
-  return "(Not found)"
+    '''Get the title text (hover tooltip text) of the given xkcd comic'''
+    try:
+        feed = feedparser.parse(XKCD_RSS_URL)
+        for item in feed["items"]:
+            item_number = re.match(URL_REGEX, item["link"], re.IGNORECASE).group(4)
+            if item_number == str(xkcd_number):
+                # Found! Get the description and parse the "title" attribute.
+                doc = html.fragment_fromstring(item["description"])
+                title_text = str(doc.attrib["title"])
+                return title_text
+    except Exception as e:
+        print "Error encountered: {0}".format(e)
+    return "(Not found)"
 
 ### Setup!
 
@@ -100,12 +102,12 @@ passf.close()
 
 submitted = []
 if os.path.isfile(SAVED_FILENAME):
-  submitted_file = open(SAVED_FILENAME, 'r')
-  submitted = submitted_file.read().split('\n')
-  submitted_file.close()
+    submitted_file = open(SAVED_FILENAME, 'r')
+    submitted = submitted_file.read().split('\n')
+    submitted_file.close()
 else:
-  submitted_file = open(SAVED_FILENAME, 'w')
-  submitted_file.close()
+    submitted_file = open(SAVED_FILENAME, 'w')
+    submitted_file.close()
 
 # Poll every 5 minutes.
 POLL_FREQUENCY = 300
@@ -113,60 +115,62 @@ POLL_FREQUENCY = 300
 ### Main Loop!
 
 try:
-  while True:
-    try:
-      logging.info("Checking /r/xkcd for new submissions...")
-      r = reddit.Reddit(user_agent=USER_AGENT)
-      r.login(USERNAME, PASSWORD)
-      submissions = r.get_subreddit('xkcd').get_new_by_date(limit=10)
-      for s in submissions:
-        matching = re.match(URL_REGEX, s.url, re.IGNORECASE)
-        #logging.info("  (Checking {0} - {1}".format(s.title, s.url))
-        if s.domain == "xkcd.com" and matching:
-          if s.url not in submitted:
-            logging.info("New xkcd submission found! {0} - {1}".format(s.title, s.url))
-            existing_comment_found = False
-            if not debug:
-              for c in s.comments:
-                if re.search("mobile version", c.body, re.IGNORECASE) or c.author == USERNAME:
-                  existing_comment_found = True
+    while True:
+        try:
+            logging.info("Checking /r/xkcd for new submissions...")
+            r = reddit.Reddit(user_agent=USER_AGENT)
+            r.login(USERNAME, PASSWORD)
+            submissions = r.get_subreddit('xkcd').get_new_by_date(limit=10)
+            for s in submissions:
+                matching = re.match(URL_REGEX, s.url, re.IGNORECASE)
+                #logging.info("  (Checking {0} - {1}".format(s.title, s.url))
+                if s.domain == "xkcd.com" and matching:
+                    if s.url not in submitted:
+                        logging.info("New xkcd submission found! {0} - {1}".format(s.title, s.url))
+                        existing_comment_found = False
+                        if not debug:
+                            for c in s.comments:
+                                if re.search("mobile version", c.body, re.IGNORECASE) or c.author == USERNAME:
+                                    existing_comment_found = True
 
-            if not existing_comment_found:
-              xkcd_number = re.match(URL_REGEX, s.url, re.IGNORECASE).group(4)
-              mobile_url = "http://m.xkcd.com/{0}/".format(xkcd_number)
-              random_string = get_fun_string()
-              
-              if ENABLE_TITLE_TEXT:
-                title_text = get_title_text(xkcd_number)
-                random_thing_to_call_the_extra_text_to_fuck_with_people = choice(["Title text", "Alt text", "Hover text", "Subtext", "Extra junk", "Mouseover text"])
-                new_comment = "**[Mobile Version!]({0})**\n\n**{1}:** {2}\n\n    (Love, xkcd_bot. {3})".format(mobile_url, random_thing_to_call_the_extra_text_to_fuck_with_people, title_text, random_string)
-              else:
-                new_comment = "**[Mobile Version!]({0})**".format(mobile_url)
+                        if not existing_comment_found:
+                            xkcd_number = re.match(URL_REGEX, s.url, re.IGNORECASE).group(4)
+                            mobile_url = "http://m.xkcd.com/{0}/".format(xkcd_number)
+                            random_string = get_fun_string()
 
-              logging.info("  -> Adding Comment!: {0}".format(new_comment))
-              retries = 0
-              while True:
-                try:
-                  retries += 1
-                  if not debug:
-                    s.add_comment(new_comment)
-                except reddit.errors.RateLimitExceeded as err:
-                  if retries < 15:
-                    logging.info("  {0} - Trying again in 60 seconds...".format(err.message))
-                    sleep(60)
-                    continue
-                break
-            # Save it!
-            if not debug:
-              submitted.append(s.url)
-              submitted_file = open(SAVED_FILENAME, 'a')
-              submitted_file.write("%s\n" % s.url)
-              submitted_file.close()
-    except urllib2.HTTPError as e:
-      logging.info("ERROR: HTTPError code {0} encountered while making request - sleeping another iteration and retrying.".format(e.code))
-    except urllib2.URLError as e:
-      logging.info("URLError: {0} - sleeping another iteration and retrying.".format(e.reason))
-    sleep(POLL_FREQUENCY)
+                            if ENABLE_TITLE_TEXT:
+                                title_text = get_title_text(xkcd_number)
+                                random_thing_to_call_the_extra_text_to_fuck_with_people = choice(["Title text", "Alt text", "Hover text", "Subtext", "Extra junk", "Mouseover text"])
+                                new_comment = "**[Mobile Version!]({0})**\n\n**{1}:** {2}\n\n    (Love, xkcd_bot. {3})".format(mobile_url, random_thing_to_call_the_extra_text_to_fuck_with_people, title_text, random_string)
+                            else:
+                                new_comment = "**[Mobile Version!]({0})**".format(mobile_url)
+
+                            logging.info("  -> Adding Comment!: {0}".format(new_comment))
+                            retries = 0
+                            while True:
+                                try:
+                                    retries += 1
+                                    if not debug:
+                                        s.add_comment(new_comment)
+                                except reddit.errors.RateLimitExceeded as err:
+                                    if retries < 15:
+                                        logging.info("  {0} - Trying again in 60 seconds...".format(err.message))
+                                        sleep(60)
+                                        continue
+                                break
+                        # Save it!
+                        if not debug:
+                            submitted.append(s.url)
+                            submitted_file = open(SAVED_FILENAME, 'a')
+                            submitted_file.write("%s\n" % s.url)
+                            submitted_file.close()
+        except urllib2.HTTPError as e:
+            logging.info("ERROR: HTTPError code {0} encountered while making request - sleeping another iteration and retrying.".format(e.code))
+        except urllib2.URLError as e:
+            logging.info("URLError: {0} - sleeping another iteration and retrying.".format(e.reason))
+        except Exception as e:
+            logging.info("Unknown error: {0} - sleeping another iteration and retrying.".format(e.reason))
+        sleep(POLL_FREQUENCY)
 except (KeyboardInterrupt):
-  logging.info('Closing %s.' % APP_TITLE)
-  quit()
+    logging.info('Closing %s.' % APP_TITLE)
+    quit()
